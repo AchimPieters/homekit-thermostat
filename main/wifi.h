2c1be8d59f47c80b0497c058e8a8e7f6835acbf1
Jozef Cipa <cipa.jozef@gmail.com>
2025-01-29 16:36:45 +0100

    feat: add wifi provisioning

--- main/wifi.h
+++ main/wifi.h
@@ -1,2 +1,11 @@
-void wifi_init(void (*on_wifi_ready)(void));
-void wifi_restart();
+#define WIFI_PROV_SEC2_USERNAME "wifiprov"
+#define WIFI_PROV_SEC2_PWD "abcd1234"
+#define PROV_MGR_MAX_RETRY_CNT 5
+#define PROV_QR_VERSION "v1"
+#define PROV_TRANSPORT_BLE "ble"
+
+void wifi_init(void);
+bool wifi_is_provisioned(void);
+void wifi_init_provisioning(char *payload, size_t payload_len);
+void wifi_connect();
+void wifi_wait_for_conn();
